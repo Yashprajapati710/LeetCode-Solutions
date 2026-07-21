aef3974c8788ee96dea0faff4b1d954aca22f611
Yash Prajapati <yashprajapati5252@gmail.com>
2026-07-21 21:36:43 +0530

    Add LeetCode Problem 383 - Ransom Note

---
+++ Easy/383_Ransom_Note.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    let map = {};
+
+for(let ch of magazine){
+    map[ch] = (map[ch] || 0) + 1;
+}
+
+for(let ch of ransomNote){
+    if(!map[ch]){
+        return false;
+    }
+
+    map[ch]--;
+}
+return true;
+
+};
