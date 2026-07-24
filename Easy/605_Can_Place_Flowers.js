8934863ac3f7a313f379a5f5baa1d73cd3876729
Yash Prajapati <yashprajapati5252@gmail.com>
2026-07-24 17:39:54 +0530

    Add LeetCode Problem 605 - Can Place Flowers

---
+++ Easy/605_Can_Place_Flowers.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let count = 0;
+
+for (i = 0; i < flowerbed.length; i++) {
+  let left = i === 0 || flowerbed[i - 1] === 0;
+
+  let right = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+
+  if (flowerbed[i] === 0 && left && right) {
+    flowerbed[i] = 1;
+    count++;
+  }
+
+  if (count >= n) {
+    return true;
+  }
+}
+return count >= n;
+
+    
+};
