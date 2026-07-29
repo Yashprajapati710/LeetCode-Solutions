545d9b7ee977cdf4a4a9c2a408babc55f89cf71f
Yash Prajapati <yashprajapati5252@gmail.com>
2026-07-29 09:22:47 +0530

    Add LeetCode Problem 1431 - Kids With Greatest Number Of Candies

---
+++ Easy/1431_Kids_With_Greatest_Number_Of_Candies.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = Math.max(...candies);
+
+let result = [];
+
+for(let candy of candies){
+    result.push(candy + extraCandies >= max);
+}
+
+return result;
+    
+};
