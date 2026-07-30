dd388343ba135b9d100970b65626a9ca8e2b0fab
Yash Prajapati <yashprajapati5252@gmail.com>
2026-07-30 18:02:53 +0530

    Add LeetCode Problem 1572 - Matrix Diagonal Sum

---
+++ Easy/1572_Matrix_Diagonal_sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+    let sum = 0;
+let n = mat.length;
+
+for(let i = 0; i < n; i++){
+
+    sum += mat[i][i];
+    sum += mat[i][n - 1 - i];
+
+}
+
+if(n%2 === 1){
+    sum -= mat[Math.floor(n / 2)][Math.floor(n/2)];
+}
+
+return sum;
+    
+};
