1317171b502213b52bd116a26bcb0615c6f9f8d0
Yash Prajapati <yashprajapati5252@gmail.com>
2026-08-11 21:38:54 +0530

    Add LeetCode Problem 71 - Simplify Path

---
+++ Medium/71_Simplify_Path.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function (path) {
+    let stack = [];
+    let parts = path.split("/");
+
+    for (let part of parts) {
+
+        if (part === "" || part === ".") {
+            continue;
+        }
+        if (part === "..") {
+            if (stack.length > 0) {
+                stack.pop();
+            }
+
+        }
+        else {
+            stack.push(part);
+        }
+    }
+
+    return "/" + stack.join("/");
+
+
+};
