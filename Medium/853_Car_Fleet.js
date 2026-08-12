2f69981f3cde14d81115999f05a72a0370626117
Yash Prajapati <yashprajapati5252@gmail.com>
2026-08-12 22:02:59 +0530

    Add LeetCode Problem 853 - Car Fleet

---
+++ Medium/853_Car_Fleet.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function (target, position, speed) {
+    let cars = [];
+
+    for (let i = 0; i < position.length; i++) {
+
+        let time = (target - position[i]) / speed[i];
+        cars.push([position[i], time]);
+    }
+
+    cars.sort((a, b) => b[0] - a[0]);
+    let stack = [];
+
+    for (let [pop, time] of cars) {
+
+        if (stack.length === 0 || time > stack[stack.length - 1]) {
+
+            stack.push(time);
+        }
+    }
+    return stack.length;
+
+};
