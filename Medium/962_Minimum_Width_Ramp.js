2e478fa5cd76c73d69a46efe8c0f861cd0f6f007
Yash Prajapati <yashprajapati5252@gmail.com>
2026-08-12 21:49:29 +0530

    Add LeetCode Problem 962 - Minimum Width Ramp

---
+++ Medium/962_Minimum_Width_Ramp.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxWidthRamp = function (nums) {
+    let stack = [];
+    let answer = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (stack.length === 0 || nums[stack[stack.length - 1]] > nums[i]) {
+
+            stack.push(i);
+        }
+    }
+
+    for (let j = nums.length - 1; j >= 0; j--) {
+
+        while (stack.length > 0 && nums[stack[stack.length - 1]] <= nums[j]) {
+
+            let i = stack.pop();
+
+            answer = Math.max(answer, j - i);
+        }
+    }
+    return answer;
+
+};
