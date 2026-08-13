926398f4f63f0849e6abadc278dddfec8e40ad52
Yash Prajapati <yashprajapati5252@gmail.com>
2026-08-13 21:41:03 +0530

    Add LeetCode Problem 1598 - Crawler Log Folder

---
+++ Easy/1598_Crawler_Log_Folder.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function (logs) {
+    let stack = [];
+
+    for (let log of logs) {
+        if (log === "../") {
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else if (log === "./") {
+            continue;
+        }
+        else {
+            stack.push(log);
+        }
+    }
+    return stack.length;
+
+};
