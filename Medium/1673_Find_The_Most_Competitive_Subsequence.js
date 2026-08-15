ca798671cdda59dadfce37642a86f0e00579123e
Yash Prajapati <yashprajapati5252@gmail.com>
2026-08-15 21:07:37 +0530

    Add LeetCode Problem 1673 - Find The Most Competitive Subsequence

---
+++ Medium/1673_Find_The_Most_Competitive_Subsequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var mostCompetitive = function (nums, k) {
+    let stack = [];
+
+    for (let i = 0; i < nums.length; i++) {
+
+        while (stack.length > 0 && stack[stack.length - 1] > nums[i] && stack.length - 1 + (nums.length - i) >= k) {
+            stack.pop();
+        }
+        stack.push(nums[i]);
+    }
+
+    return stack.slice(0, k);
+
+};
