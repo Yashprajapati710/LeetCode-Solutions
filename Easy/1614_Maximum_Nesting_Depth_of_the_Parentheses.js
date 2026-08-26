d668020564cd7d250f7dd94affab3ac18f997181
Yash Prajapati <yashprajapati5252@gmail.com>
2026-08-26 18:32:34 +0530

    Add LeetCode Problem 1614 - Maximum Nesting Depth of the Parentheses

---
+++ Easy/1614_Maximum_Nesting_Depth_of_the_Parentheses.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function (s) {
+    let depth = 0;
+    let maxDepth = 0;
+
+    for (let ch of s) {
+        if (ch === "(") {
+            depth++;
+
+            maxDepth = Math.max(maxDepth, depth);
+        } else if (ch === ")") {
+            depth--;
+        }
+    }
+
+    return maxDepth;
+};
